c5fdae17b23839e051c0d12c67aa4e7773238a0c
Ebru Çakar <ebru_cakar_11@hotmail.com>
2020-07-27 19:56:54 +0300

    First create.
    İlk proje dosyaları.

---
+++ Assets/Scripts/Deneme.cs
@@ -0,0 +1,41 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Deneme : MonoBehaviour
+{
+    public int x, y;
+    // Değişken tanımlama kuralı
+    // erişilebilirlik + değişken türü + değişken ismi
+    //  public
+    //  private
+    //  protected
+
+    //  int // tam sayı
+    //  float // virgüllü sayı
+    //  string // kelimeler harfer karakterler
+    //  bool // True ve false
+    //  GameObject
+    //  Animation
+    //  var 
+
+    // Fonksiyon tanımlama kuralı
+    // erişebilirlik + dönecek değişken türü + fonksiyon ismi+ (parameteler) + { ana blok }
+    // void geriye değer döndürmez
+
+    Image image;
+
+    private void Start()
+    {
+        int deger = 5;
+        Debug.Log(toplama(x, y));
+        Debug.Log(deger);
+    }
+
+    public int toplama(int sayi1, int sayi2)
+    {
+        return sayi1 + sayi2;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 5;
+    int count = 0;
+    public int x;
+    public Text ScoreText, WinText;
+    int TargetCount = 3;
+    public FixedJoystick joystick;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        //float horizontal = Input.GetAxis("Horizontal");
+        //float vertical = Input.GetAxis("Vertical");
+
+        float horizontal = joystick.Horizontal;
+        float vertical = joystick.Vertical;
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+
+        rb.AddForce(movement * speed);       
+
+        //Touch touch = Input.GetTouch(0);
+        //touch.phase;
+        //TouchPhase.
+        //Input.touchCount
+        //Input.touches
+        //touch.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //other.gameObject.tag == "Collectible"
+        if (other.gameObject.CompareTag("Collectible"))
+        {
+            //objeyi topla
+            //collision.gameObject.SetActive(false);
+            Destroy(other.gameObject);
+            //count = count + 1;
+            //count += 1;
+            count++;
+            ScoreText.text = "Score : " + count.ToString();
+            if (count >= TargetCount)
+            {
+                // Oyunu kazandık
+                WinText.gameObject.SetActive(true);
+            }
+        }
+    }
+
+}
